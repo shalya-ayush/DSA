21fdb42c3e6ed63f863995b4adf872e29688b1f7
Ayush Shalya <shalyaayush@gmail.com>
2025-08-03 02:16:38 +0530

    Time: 23 ms (46.64%) | Memory: 17.8 MB (82.56%) - LeetSync

--- 383-ransom-note/ransom-note.py
+++ 383-ransom-note/ransom-note.py
@@ -1,38 +1,22 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
 
-        ransomNoteMap = {}
-        magazineMap = {}
+        # Create a hashmap for magazine
+        magazine_hash = {}
+        for i in magazine:
+            magazine_hash[i] = 1 + magazine_hash.get(i,0)
 
-        for i in range(len(ransomNote)):
-            if ransomNote[i] in ransomNoteMap:
-                # print(ransomNoteMap.get(ransomNote[i]) + 1)
-                ransomNoteMap.update({ransomNote[i]:ransomNoteMap.get(ransomNote[i]) +1})
-            else:
-                ransomNoteMap.update({ransomNote[i]:1})
-        
-        for i in range(len(magazine)):
-            if magazine[i] in magazineMap:
-                # pass
-                magazineMap.update({magazine[i]:magazineMap.get(magazine[i])+1})
-            else:
-                magazineMap.update({magazine[i]:1})
-        
-        # Compare 2 dictionaries of different length
-    #    / print(ransomNoteMap)
-        # print(magazineMap)
-        common_keys = ransomNoteMap.keys() & magazineMap.keys()
-        print(common_keys)
-
-        if not common_keys or len(common_keys) < len(ransomNoteMap.keys()):
-            return False
-        
-        for key in common_keys:
-            if ransomNoteMap[key] > magazineMap[key]:
+        # Now iterate over ransomeNote string and subtract the value of chracter from the 
+        # magazine hash dictionary if the the character exists in the magazine_hash
+        # dictionary
+        for i in ransomNote:
+            if i not in magazine_hash or magazine_hash[i] <= 0:
                 return False
+            
+            magazine_hash[i] -=1
 
+        
         return True
-
 
 
         
